fc1a73a259f82d4c1ad35ed764abc06f4ec6f3fc
Akyas-S <akyas.s.official@gmail.com>
2024-07-05 21:50:00 +0800

    clean up and character files

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -5,6 +5,7 @@ import Inputs.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.Random;
 
 import static java.lang.Math.random;
@@ -13,24 +14,23 @@ import static java.lang.Math.random;
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
-    float PlayerX = 0;
-    float PlayerY = 0  ;
-    int frames;
-    int red, green, blue;
-    private long lastCheck;
-    float xDir = 5f;
-    float yDir = 5f;
-    Random random;
-
+    private float PlayerX = 0;
+    private float PlayerY = 0  ;
+    private BufferedImage Image;
 
     public GamePanel(){
 
-        random = new Random();
         mouseInputs = new MouseInputs(this);
+        setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
 
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 720);
+        setPreferredSize(size);
     }
 
     // Takes keyboard inputs and moves player
@@ -39,44 +39,12 @@ public class GamePanel extends JPanel {
         this.PlayerY += Y;
 
     }
-    // Tracks movement of mouse and maps it to player
-    public void mouseMove(int X, int Y){
-        this.PlayerX = X;
-        this.PlayerY = Y;
-
-
-    }
 
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        updateRect();
-
-        g.setColor(new Color(red,green,blue));
-        g.fillRect((int)PlayerX, (int)PlayerY, 50, 50);
-
-
-
+        g.drawImage()
 
     }
 
-    private void updateRect() {
-        PlayerX += xDir;
-        if (PlayerX >= 1280-50 || PlayerX <0){
-            rndcolor();
-            xDir *=-1;
-        }
-        PlayerY += yDir;
-        if (PlayerY >= 720-50 || PlayerY < 0){
-            rndcolor();
-            yDir *=-1;
-        }
-
-    }
-
-    private void rndcolor() {
-        red = random.nextInt(256);
-        green = random.nextInt(256);
-        blue = random.nextInt(256);
-    }
 }
--- src/main/Window.java
+++ src/main/Window.java
@@ -8,10 +8,11 @@ import javax.swing.*;
 
     public Window(JPanel jpanel){
         jframe = new JFrame();
-        jframe.setSize(1280,720);
+
         jframe.add(jpanel);
         jframe.setLocationRelativeTo(null);
         jframe.setResizable(false);
+        jframe.pack();
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.setVisible(true);
     }
