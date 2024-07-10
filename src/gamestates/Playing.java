08dd6e900442362f49304fba21454bc475297f8f
Hybrid-22 <96981863+Hybrid-22@users.noreply.github.com>
2024-07-11 01:07:39 +0800

    Add files via upload

---
+++ src/gamestates/Playing.java
@@ -0,0 +1,94 @@
+package gamestates;
+
+import entity.EnemyManager;
+import entity.Player;
+import levels.Level1;
+import main.GameController;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.util.Random;
+
+public class Playing extends State implements Statemethods {
+    private Player player;
+    private EnemyManager enemyManager;
+    private Random random = new Random();
+
+    public Playing(GameController game) {
+        super(game);
+        initClasses();
+        update();
+    }
+
+    private void initClasses() {
+
+        player = new Player(200,200);
+        enemyManager = new EnemyManager(this,player);
+    }
+
+    public Player getPlayer(){
+        return player;
+    }
+
+    @Override
+    public void render(Graphics g) {
+        Level1.render(g);
+        player.render(g);
+        enemyManager.draw(g);
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int temp = e.getKeyCode();
+        if (temp == KeyEvent.VK_W){
+            player.setUp(true);}
+        if (temp == KeyEvent.VK_A){
+            player.setLeft(true);}
+        if (temp == KeyEvent.VK_D){
+            player.setRight(true);}
+        if (temp == KeyEvent.VK_S){
+            player.setDown(true);}
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int temp = e.getKeyCode();
+        if (temp == KeyEvent.VK_W){
+            player.setUp(false);}
+        if (temp == KeyEvent.VK_A){
+            player.setLeft(false);}
+        if (temp == KeyEvent.VK_D){
+            player.setRight(false);}
+        if (temp == KeyEvent.VK_S){
+            player.setDown(false);}
+    }
+
+    public void update() {
+
+        enemyManager.update();
+    }
+}
