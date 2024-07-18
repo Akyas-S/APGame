dd9bddb8daf2e87c64188bd6e32dcc560e7cd7af
ammurizny <ammurizny@gmail.com>
2024-07-19 02:22:40 +0800

    stuff

--- src/gamestates/LevelCompleted.java
+++ src/gamestates/LevelCompleted.java
@@ -1,21 +1,91 @@
 package gamestates;
 
+import main.GameController;
+import utils.LoadImages;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
-public class LevelCompleted {
+public class LevelCompleted extends State implements Statemethods {
+    private BufferedImage background;
 
-    private Playing playing;
-    private UrmButton menu, next;
-    private BufferedImage img;
-    private int bgX, bgY, bgW, bgH;
+    private BufferedImage Menuicon;
+    private BufferedImage Replayicon;
+    private BufferedImage Nexticon;
 
-    public LevelCompleted(Playing playing) {
-        this.playing = playing;
-        initImg();
+    private Rectangle menuButtonBounds;
+    private Rectangle replayButtonBounds;
+
+    public LevelCompleted(GameController game) {
+        super(game);
+        loadBackground();
+        loadButtons();
     }
 
-    private void initImg() {
+    private void loadBackground() {background = LoadImages.GetSprite(LoadImages.WON_BG);
     }
 
+    private void loadButtons() {
+        Menuicon = LoadImages.GetSprite(LoadImages.MENU_BUTTON);
+        Replayicon = LoadImages.GetSprite(LoadImages.REPLAY_BUTTON);
+
+        menuButtonBounds = new Rectangle(500, 200, 294, 91);
+        replayButtonBounds = new Rectangle(500, 310, 294, 91);
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.drawImage(background, 0,0,1280,720,null);
+        g.drawImage(Menuicon, 500,200,294, 91,null);
+        g.drawImage(Replayicon, 500,310,294, 91, null);
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        Point clickPoint = e.getPoint();
+        if (menuButtonBounds.contains(clickPoint)) {
+            Gamestate.state = Gamestate.MENU;
+            game.getAudioPlayer().playMenuButtonSound();
+        } else if (storeButtonBounds.contains(clickPoint)) {
+            Gamestate.state = Gamestate.STORE;
+            game.getAudioPlayer().playMenuButtonSound();
+        } else if (settingsButtonBounds.contains(clickPoint)) {
+            Gamestate.state = Gamestate.SETTINGS;
+            game.getAudioPlayer().playMenuButtonSound();
+        } else if (exitButtonBounds.contains(clickPoint)) {
+            game.getAudioPlayer().playMenuButtonSound();
+            System.exit(0);
+        }
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
 
