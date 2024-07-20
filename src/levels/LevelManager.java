e6f82d5f674f5ec9c0d448158a502d49fca3da3e
Akyas-S <akyas.s.official@gmail.com>
2024-07-20 18:41:42 +0800

    removed level 3

--- src/levels/LevelManager.java
+++ src/levels/LevelManager.java
@@ -22,14 +22,10 @@ public class LevelManager {
     private Level1 level1;
     // Level 2 object
     private Level2 level2;
-    // Level 3 object
-    private Level3 level3;
     // Boolean to track which level is currently active
     private boolean isLevel1;
     // Boolean to track which level is currently active
     private boolean isLevel2 = false;
-    // Boolean to track which level is currently active
-    private boolean islevel3 = false;
     // SaveLoad object to handle saving and loading game data
     private SaveLoad saveLoad;
 
@@ -42,7 +38,7 @@ public class LevelManager {
         // Create level objects
         this.level1 = new Level1(playing, player);
         this.level2 = new Level2(playing, player);
-        this.level3 = new Level3(playing, player);
+
         // Set initial level to Level 1
         this.isLevel1 = true;
         // Create SaveLoad object to handle saving and loading game data
@@ -72,25 +68,13 @@ public class LevelManager {
             // If player score reaches 50, move to Level 3
             if (player.playerScore >= 50) {
                 isLevel2 = false;
-                islevel3 = true;
                 // Calculate and save coins earned in Level 2
                 calcuateCoins(player.playerScore);
                 saveLoad.saveCoins();
                 System.out.println("Total coins: " + player.playerTotalCoins);
             }
         }
-        // If Level 3 is active, render it and check for level completion
-        if(islevel3){
-            level3.render(g);
-            // If player score reaches 200, level is complete
-            if(player.playerScore == 200){
-                islevel3 = false;
-                // Calculate and save coins earned in Level 3
-                calcuateCoins(player.playerScore);
-                saveLoad.saveCoins();
-                System.out.println("Total coins: " + player.playerTotalCoins);
-            }
-        }
+
     }
 
     // Calculate coins earned based on player score
